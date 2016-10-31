2345f717de95440330f48963ddde31f3a5d99542
Luca Vallerini <lucavall90@gmail.com>
2016-10-31 10:05:37 +0100

    Added comments, licence and removed the print out of the word to guess

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,7 +1,40 @@
+# A basic The Hangman game in Python.
+
+# This code is licensed under the MIT License.
+#
+# MIT License
+#
+# Copyright (c) 2016 Luca Vallerini
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Author: Luca Vallerini
+# E-mail: lucavall90@gmail.com
+#
+# Date: 2016-10-30
+# Last update: 2016-10-31
+
 import random
 from sys import exit
 
 
+# Choose a random word from a dictionary
 def choose_a_word(lang):
     with open(lang + '.txt', 'r') as dictionary:
         lines = dictionary.readlines()
@@ -9,10 +42,11 @@ def choose_a_word(lang):
     return random.choice(lines).strip().upper()
 
 
+# Start the game with a given player name and
+# a dictionary choose by the player.
 def game(player, lang):
-    word = choose_a_word(lang)  # italian, english
+    word = choose_a_word(lang)
     show_word = "_" * len(word)
-    print word
 
     turn = 0
     guesses = ""
@@ -45,6 +79,8 @@ def game(player, lang):
         hangman(turn, show_word, guesses)
 
 
+# Print The Hangman board, spaces for the word to guess
+# and the guesses done so far.
 def hangman(turn, word, guesses):
     print "      _____"
     print "      |   |"
@@ -79,6 +115,8 @@ def hangman(turn, word, guesses):
     print "Already guessed: " + guesses + "\n"
 
 
+# Start the game asking the player for his/her name
+# and let the player choose for the dictionary to play with.
 def start():
     print "Hi! Welcome to The Hangman game!"
     player = raw_input("What's your name? ").strip().upper()
